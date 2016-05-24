872db89b648b87778c90115f0c1aa9cafc240f13
Rishit Sinha <rishitsinha25@gmail.com>
2016-05-25 05:03:39 +0530

    Added Goalkeeper And Defense - Marking Tactics

---
+++ tGoalie.cpp
@@ -0,0 +1,188 @@
+#include <list>
+#include "tactics/tGoalie.hpp"
+#include "skills/skillSet.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <ssl_common/config.h>
+#include <iostream>
+#include <stdio.h>
+#include <ssl_common/geometry.hpp>
+
+#include <fstream>
+
+#define KICK_RANGE_THRESH MAX_DRIBBLE_R
+#define THRES (0.8f)
+#define THETA_THRESH 0.005
+
+float getRandom() ;
+
+
+namespace Strategy{
+
+	TGoalie::TGoalie(int botID):
+		Tactic(botID){
+
+		}
+	
+	TGoalie::~TGoalie(){
+
+	}
+
+	bool TGoalie::isCompleted(const BeliefState &bs) const{
+		//add logic
+		return false;
+	}
+	bool TGoalie::isActiveTactic(void)const{
+		return false;
+	}
+
+	int TGoalie::chooseBestBot(const BeliefState &state, std::list<int>& freeBots, const Param& tParam, int prevID) const{
+		
+	}
+
+	gr_Robot_Command TGoalie::execute(const BeliefState &state, const Tactic::Param& tParam){
+
+		fstream myfile;
+
+		myfile.open ("/home/rishit/Desktop/LOG.txt" , fstream::app);
+		
+		myfile << "\n ------------------------------------------------------- \n";
+		
+
+
+		Vector2D<int> ballAim, goalieTarget;
+		Vector2D<int> botpos (state.homePos[botID].x, state.homePos[botID].y);
+		Vector2D<int> ballPos(state.ballPos.x, state.ballPos.y);
+
+		Vector2D<int> ballVel(state.ballVel.x , state.ballVel.y);
+
+		Strategy::SkillSet::SkillID sID;
+		SkillSet::SParam sParam;
+		
+		
+		float dist = Vector2D<int>::dist(ballPos, botpos);
+		if(ballVel.absSq() < MAX_BOT_SPEED && abs(state.ballPos.y) < OUR_GOAL_MAXY && state.ballPos.x  > (HALF_FIELD_MAXX - 2 * DBOX_WIDTH)){	//Invert sign on Side Change
+			if( state.ballPos.x > state.homePos[botID].x ){
+				//float r_theta = atan2( (state.ballPos.y - state.homePos[botID].y) , (state.ballPos.x - state.homePos[botID].x));	
+				/*if( state.homePos[botID].theta != r_theta){
+					sID = SkillSet::TurnToAngle;
+					sParam.TurnToAngleP.x = state.ballPos.x;
+					sParam.TurnToAngleP.y = state.ballPos.y;
+					sParam.TurnToAngleP.radius = 0;	myfile << " ballPos: ( " << state.ballPos.x << " " << state.ballPos.y << " )\tBotangle: " << state.homePos[botID].theta << " \n TurnToPoint executed" ; 
+					return SkillSet::instance()->executeSkill(sID, sParam, state, botID);
+				}
+				else */if( dist >= DRIBBLER_BALL_THRESH){
+					sID = SkillSet::GoToBall;						myfile << " ballPos: ( " << state.ballPos.x << " " << state.ballPos.y << " ) \n GoToBall executed" ; 
+					return SkillSet::instance()->executeSkill(sID, sParam, state, botID);	 
+				}
+				else{
+					sID = SkillSet::DribbleToPoint;
+					sParam.DribbleToPointP.x = state.homePos[botID].x;
+					sParam.DribbleToPointP.y = state.homePos[botID].y;
+					sParam.DribbleToPointP.finalslope = 0;
+					sParam.DribbleToPointP.radius = 0;					myfile << "trying to pull" << endl;
+					return SkillSet::instance()->executeSkill(sID,sParam,state,botID);
+				}
+			}
+			else if( dist >= DRIBBLER_BALL_THRESH){
+				sID = SkillSet::GoToBall;						myfile << " ballPos: ( " << state.ballPos.x << " " << state.ballPos.y << " ) \n GoToBall executed" ; 
+				return SkillSet::instance()->executeSkill(sID, sParam, state, botID);	 
+			}
+			else{
+				sID = SkillSet::Kick;
+				sParam.KickP.power = 7.0f;							myfile << " ballPos: ( " << state.ballPos.x << " " << state.ballPos.y << " ) \n Kick executed" ; 
+				return SkillSet::instance()->executeSkill(sID, sParam, state, botID);    	
+			}
+
+		}
+		
+		float default_x = HALF_FIELD_MAXX - DBOX_WIDTH/2;														//Invert Signs on Side Change
+
+		if(state.ballPos.x > default_x && state.ballPos.x < HALF_FIELD_MAXX)												//Invert Signs on Side Change
+			goalieTarget.x = state.ballPos.x + BOT_RADIUS + BALL_RADIUS;												//Invert Signs on Side Change
+		else
+			goalieTarget.x = default_x;
+		
+
+		float striker = -1 , striker_dist = 99999999999;
+
+
+		for(int oppID=0;oppID<4;++oppID){
+			Vector2D<int> oppPos (state.awayPos[oppID].x, state.awayPos[oppID].y);	
+
+			float kick_range_test = (oppPos - ballPos).absSq();
+
+			if(kick_range_test < KICK_RANGE_THRESH && kick_range_test < striker_dist){
+				striker = oppID;
+				striker_dist = kick_range_test;
+			}
+		}
+
+		if(striker != -1){
+			goalieTarget.y = ( ((state.ballPos.y - state.awayPos[striker].y) / (state.ballPos.x - state.awayPos[striker].x)) * (goalieTarget.x - state.ballPos.x) ) + state.ballPos.y;
+		}
+
+		else{
+			if(state.ballVel.x == 0){
+				goalieTarget.y = state.ballPos.y;
+			}
+			else{
+				if(state.ballVel.x > 0)
+					goalieTarget.y 	= (( state.ballVel.y / state.ballVel.x ) * ( goalieTarget.x - state.ballPos.x ) ) + state.ballPos.y;
+				else
+					goalieTarget.y 	= 0;
+			}
+		}
+
+
+		
+		if(goalieTarget.y< OUR_GOAL_MINY/1.2){
+			goalieTarget.y = OUR_GOAL_MINY/1.2;
+		}
+		else if(goalieTarget.y > OUR_GOAL_MAXY/1.2){
+			goalieTarget.y = OUR_GOAL_MAXY/1.2;
+		}
+
+
+		
+
+
+		sID = SkillSet::GoToPoint;
+		sParam.GoToPointP.x = goalieTarget.x;													myfile << "\tx=" << goalieTarget.x << endl;
+
+		sParam.GoToPointP.y = goalieTarget.y;													myfile << "\ty=" << goalieTarget.y << endl;
+	 	
+	 	sParam.GoToPointP.finalVelocity = 0;													myfile << "\tvel = 0" << endl;
+	 	
+	 	sParam.GoToPointP.finalslope = atan2( (state.ballPos.y - state.homePos[botID].y) , (state.ballPos.x - state.homePos[botID].x));		myfile << "\tslope = sParam.GoToPointP.finalslope" << endl;
+	 																
+	 																		myfile << "GoToPoint executed" << endl;
+
+	 	return SkillSet::instance()->executeSkill(sID, sParam, state, botID);							
+
+
+	}
+	
+	
+
+ 	 Tactic::Param TGoalie::paramFromJSON(string json) {
+	      using namespace rapidjson;
+	      Tactic::Param tParam;
+	      
+	      return tParam;
+	}
+
+    	string TGoalie::paramToJSON(Tactic::Param tParam) {
+	      return string("");
+	}
+	    
+}
+
+float getRandom() {
+	float randValue = (float)rand() / RAND_MAX + 0.000001f;
+	if (randValue >= 1.0f) {
+		randValue = 0.999999f;
+	}
+	return randValue;
+}
---
+++ tMark.cpp
@@ -0,0 +1,159 @@
+#include <list>
+#include "tactics/tMark.hpp"
+#include "skills/skillSet.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <ssl_common/config.h>
+#include <iostream>
+#include <stdio.h>
+#include <ssl_common/geometry.hpp>
+
+#include <fstream>
+
+#define KICK_RANGE_THRESH MAX_DRIBBLE_R
+#define THRES (0.8f)
+#define THETA_THRESH 0.005
+#define TURNING_THRESH 10
+
+namespace Strategy{
+
+	TMark::TMark(int botID):
+		Tactic(botID){
+
+		}
+	
+	TMark::~TMark(){
+
+	}
+
+	bool TMark::isCompleted(const BeliefState &bs) const{
+		//add logic
+		return false;
+	}
+	bool TMark::isActiveTactic(void)const{
+		return false;
+	}
+
+	int TMark::chooseBestBot(const BeliefState &state, std::list<int>& freeBots, const Param& tParam, int prevID) const{
+		
+	}
+
+	gr_Robot_Command TMark::execute(const BeliefState &state, const Tactic::Param& tParam){
+
+
+
+
+		fstream myfile;
+
+		myfile.open ("/home/rishit/Desktop/mark.txt" , fstream::app);
+		
+		
+
+
+
+
+
+		Vector2D<int> ballAim, marked, guardPos;
+		Vector2D<int> botpos (state.homePos[botID].x, state.homePos[botID].y);
+		Vector2D<int> ballPos(state.ballPos.x, state.ballPos.y);
+		Vector2D<int> markedPos (state.homePos[0].x, state.homePos[0].y);
+		Vector2D<int> ballVel(state.ballVel.x , state.ballVel.y);
+		
+		Strategy::SkillSet::SkillID sID;
+		SkillSet::SParam sParam;
+	
+		
+	
+		float passer = -1 , passer_dist = 99999999999;
+
+
+		for(int oppID=0;oppID<4;++oppID){
+			Vector2D<int> oppPos (state.homePos[oppID].x, state.homePos[oppID].y);
+			float kick_range_test = (oppPos - ballPos).absSq();
+
+			if(kick_range_test < KICK_RANGE_THRESH && kick_range_test < passer_dist){
+				passer = oppID;
+				passer_dist = kick_range_test;
+			}
+		}
+
+		if(passer != -1){
+			if( abs(ballPos.x - markedPos.x) > abs(ballPos.y - markedPos.y)){
+				if(ballPos.x > markedPos.x)
+					guardPos.x = markedPos.x + DBOX_WIDTH/2;
+				else
+					guardPos.x = markedPos.x - DBOX_WIDTH/2;
+				
+				guardPos.y = ( ((state.ballPos.y - state.awayPos[passer].y) / (state.ballPos.x - state.awayPos[passer].x)) * (guardPos.x - state.ballPos.x) ) + state.ballPos.y;
+			}
+			else{
+				if(ballPos.y > markedPos.y)
+					guardPos.y = markedPos.y + DBOX_WIDTH/2;
+				else
+					guardPos.y = markedPos.y - DBOX_WIDTH/2;
+				
+				guardPos.x = ( ((state.ballPos.x - state.awayPos[passer].x) / (state.ballPos.y - state.awayPos[passer].y)) * (guardPos.y - state.ballPos.y) ) + state.ballPos.x;
+			}
+		}
+
+		else{
+			if( abs(ballPos.x - markedPos.x) > abs(ballPos.y - markedPos.y)){
+				if(ballPos.x > markedPos.x)
+					guardPos.x = markedPos.x + DBOX_WIDTH/2;
+				else
+					guardPos.x = markedPos.x - DBOX_WIDTH/2;
+				
+				guardPos.y = ( ((markedPos.y - state.ballPos.y) / (markedPos.x - state.ballPos.x)) * (guardPos.x - state.ballPos.x) ) + state.ballPos.y;
+			}
+			else{
+				if(ballPos.y > markedPos.y)
+					guardPos.y = markedPos.y + DBOX_WIDTH/2;
+				else
+					guardPos.y = markedPos.y - DBOX_WIDTH/2;
+				
+				guardPos.x = ( ((markedPos.x - state.ballPos.x) / (markedPos.y - state.ballPos.y)) * (guardPos.y - state.ballPos.y) ) + state.ballPos.x;
+			}
+		}
+		
+
+		if( (botpos - guardPos).absSq() < TURNING_THRESH ){
+			sID = SkillSet::TurnToPoint;
+			sParam.TurnToPointP.x = guardPos.x;
+			sParam.TurnToPointP.y = guardPos.y;
+			sParam.TurnToPointP.max_omega = 5 * MAX_BOT_OMEGA;
+			myfile << "tryna turn" << endl;
+			return SkillSet::instance()->executeSkill(sID, sParam, state, botID);
+		}
+
+		sID = SkillSet::GoToPoint;
+		sParam.GoToPointP.x = guardPos.x;
+
+		sParam.GoToPointP.y = guardPos.y;
+	 	
+	 	sParam.GoToPointP.finalVelocity = 0;	
+	 	
+	 	float angleToTurn = normalizeAngle(Vector2D<int>::angle(ballPos , guardPos));									
+	 	sParam.GoToPointP.finalslope = angleToTurn ;
+	 	
+	 																	
+
+	 	return SkillSet::instance()->executeSkill(sID, sParam, state, botID);							
+
+
+	}
+	
+	
+
+ 	 Tactic::Param TMark::paramFromJSON(string json) {
+	      using namespace rapidjson;
+	      Tactic::Param tParam;
+	      
+	      return tParam;
+	}
+
+    	string TMark::paramToJSON(Tactic::Param tParam) {
+	      return string("");
+	}
+	    
+}
